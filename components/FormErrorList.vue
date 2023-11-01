81258e4874eaebef5bd40df3ec97daffbdb48274
joe-irving <joeirving.jbi@gmail.com>
2023-11-01 13:56:53 +0000

    Trigger signin via majic link from bottom of form

---
+++ components/FormErrorList.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { FormValidationError } from 'naive-ui'
+import { PropType } from 'nuxt/dist/app/compat/capi'
+
+defineProps({
+  errors: {
+    type: Array as PropType<FormValidationError[]>,
+    default: () => []
+  }
+})
+</script>
+
+<template>
+  <n-steps :vertical="true">
+    <n-step v-for="(error, i) in errors.flat()" :key="i" :title="error.message" status="error" />
+  </n-steps>
+</template>
--- pages/petition/create/[id].vue
+++ pages/petition/create/[id].vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
-import { StepsProps, InputInst, FormRules, FormItemRule } from 'naive-ui'
+import { StepsProps, InputInst, FormRules, FormItemRule, FormValidationError } from 'naive-ui'
+const { $client } = useNuxtApp()
+const { signIn } = useAuth()
+const route = useRoute()
+
+const { data: user } = $client.user.me.useQuery()
 
 const { $i18n } = useNuxtApp()
 // Get id prop, if it is an int then get the campaign, if not create without link to campaign?
@@ -8,21 +13,22 @@ const currentRef = ref<number>(1)
 const currentStatus = ref<StepsProps['status']>('process')
 const totalSteps = ref(4)
 
+// Element references
 const title = ref<HTMLElement | null>(null)
 const titleInput = ref<InputInst | null>(null)
-
 const description = ref<HTMLElement | null>(null)
 // const descriptionInput = ref<InputInst | null>(null)
 const image = ref<HTMLElement | null>(null)
 // const imageInput = ref<InputInst | null>(null)
 const email = ref<HTMLElement | null>(null)
 const emailInput = ref<InputInst | null>(null)
+const formRef = ref<FormInst | null>(null)
 
 const petition = ref({
-  title: '',
-  description: 'Example description',
+  title: route.query.title?.toString() || '',
+  description: route.query.content?.toString() || '',
   image: [],
-  email: ''
+  email: route.query.email?.toString() || ''
 })
 
 // const emailStatus = computed(() => {
@@ -69,10 +75,12 @@ const formRules = ref<FormRules>({
       } else if (!/^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i.test(value)) {
         return new Error($i18n.t('petition_create.email_validator'))
       }
-      return true
+      return true //  i couldn't work out to remove the rule param from the function
     }
   }
 })
+
+const formWarningMessages = ref<FormValidationError[]>([])
 
 const goToStep = (stepNumber: number) => {
   currentRef.value = stepNumber
@@ -91,6 +99,34 @@ const prevStep = () => {
   }
 }
 
+const handleCreatePetition = () => {
+  // Check if all values needed are entered, if not show clear error message about what is missing.
+  formWarningMessages.value = []
+  formRef.value?.validate((errors: Array<FormValidationError> | undefined) => {
+    if (errors) {
+      console.log(errors)
+      formWarningMessages.value = errors
+    } else {
+      console.log('creating petition')
+      createPetition()
+    }
+  })
+}
+
+const createPetition = async () => {
+  // const petitionCreated = $client
+
+  const res = await signIn('sendgrid', {
+    email: petition.value.email,
+    callbackUrl: '/petition/1',
+    redirect: false
+  })
+  // await signIn('google')
+  // Create petition in back end, with either user email attached or linked to logged in userEmail
+  // if not logged in -> take them to login page -> redirect to manage page
+  // if logged in -> redirect to manage page once created
+}
+
 definePageMeta({
   layout: 'public',
   auth: false
@@ -99,7 +135,7 @@ definePageMeta({
 
 <template>
   <div class="main-content">
-    <n-form :rules="formRules" :model="petition">
+    <n-form ref="formRef" :rules="formRules" :model="petition">
       <n-steps v-model:current="currentRef" :vertical="true" :status="currentStatus">
         <n-step :title="$t('petition_create.title')">
           <span ref="title" />
@@ -132,13 +168,22 @@ definePageMeta({
             <ImageUpload @change="(fileList) => petition.image = fileList" />
           </div>
         </n-step>
-        <n-step :title="$t('petition_create.email_title')">
+        <n-step :title="user?.authenticated ? $t('petition_create.create_button') : $t('petition_create.email_title')">
+          <!-- Only if not signed in, otherwise just show button -->
           <div ref="email" class="n-step-description full">
-            <n-p>{{ $t("petition_create.email_description") }}</n-p>
-            <n-form-item path="email">
-              <n-input ref="emailInput" v-model:value="petition.email" type="text" />
-            </n-form-item>
-            <n-button>{{ $t('petition_create.create_button') }}</n-button>
+            <div v-if="!user?.authenticated">
+              <n-p>{{ $t("petition_create.email_description") }}</n-p>
+              <n-form-item path="email">
+                <n-input ref="emailInput" v-model:value="petition.email" type="text" />
+              </n-form-item>
+            </div>
+            <!-- , { username: petition.email, callbackUrl: '/peition' }) -->
+            <n-button @click="handleCreatePetition()">
+              {{ $t('petition_create.create_button') }}
+            </n-button>
+            <div v-if="formWarningMessages.length > 0">
+              <FormErrorList :errors="formWarningMessages" />
+            </div>
           </div>
         </n-step>
       </n-steps>
@@ -150,7 +195,7 @@ definePageMeta({
         </n-button>
       </div>
       <div class="right">
-        <n-button v-if="currentRef < totalSteps" @click="nextStep()">
+        <n-button v-if="currentRef < totalSteps" @click.prevent="nextStep()">
           {{ $t("petition_create.nav_next") }}
         </n-button>
       </div>
