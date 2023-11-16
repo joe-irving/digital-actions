2fae680ded4c8319f6c0db9d699e2a1ef2b6afdb
joe-irving <joeirving.jbi@gmail.com>
2023-11-16 16:46:27 +0000

    Add image upload to google cloud

--- server/api/upload.post.ts
+++ server/api/upload.post.ts
@@ -1,15 +1,23 @@
-import fs from 'fs'
-import path from 'path'
+// import fs from 'fs'
+// import path from 'path'
 import { readFiles } from 'h3-formidable'
+import { Storage } from '@google-cloud/storage'
+
+const storage = new Storage({
+  projectId: process.env.GOOGLE_PROJECT_ID,
+  keyFilename: 'google-key.json'
+})
+const bucket = storage.bucket(process.env.GOOGLE_BUCKET_ID || '')
 
 export default defineEventHandler(async (event) => {
   const { files } = await readFiles(event)
 
   const imageName = String(Date.now()) + String(Math.round(Math.random() * 10000000))
   const fileName = `${imageName}.${files.file[0].mimetype.split('/')[1]}`
-  const newPath = `${path.join('public', 'uploads', fileName)}`
-  const publicPath = `/uploads/${fileName}`
-  fs.copyFileSync(files.file[0].filepath, newPath)
+  const uploadedFile = await bucket.upload(files.file[0].filepath, {
+    destination: fileName,
+    public: true
+  })
 
-  return { success: true, path: publicPath, name: fileName }
+  return { success: true, path: uploadedFile[0].metadata.mediaLink, name: fileName }
 })
